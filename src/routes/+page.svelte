99026f21e20f8024e08083a4b23b20e8987fde70
Paloma Borro <paloma.borro@wizeline.com>
2023-11-20 13:29:33 +0100

    Add new svelte project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Lo que está renderizando</h1>
+
+<style>
+    h1 {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+</style>
